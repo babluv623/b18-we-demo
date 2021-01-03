86b46afe5a04d7effd4de79b346a7a9fda3a89ac
Venkatasubramanian <pvenkat.rv@gmail.com>
2021-01-03 16:17:47 +0530

    some changes

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import { Car } from "./car";
 import { carConfiguration } from "./car-config";
 
 
-let obj: carConfiguration = { carValue: 'Cretra', brandValue: 'Hyundai', colorValue: 'RED', trans: true, make: 2021 };
+let obj: carConfiguration = { carValue: 'EON', brandValue: 'Hyundai', colorValue: 'RED', trans: true, make: 2021 };
 
 let c = new Car(obj);
 
